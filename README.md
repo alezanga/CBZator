61deac704fa2de07da919e62696fffddb85dba93
ALIENK9 <alezanga@outlook.it>
2020-04-05 13:54:50 +0200

    Handles spaces, no more detox, uppercase ext

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A very simple bash script to convert folders of images into a single `cbz` archi
 
 ## Dependencies
 
-The script needs `detox` installed.
+Only `zip` and `unzip` packages (and `coreutils`).
 
 ## Usage
 
@@ -13,17 +13,15 @@ The script needs `detox` installed.
 
 To generate a `cbz` archive from images in input directory: `./cbzator.sh in_dir out_dir`.
 
-- `in_dir` must be a folder containing `.jpg` files OR a folder of folders containing `.jpg` files. To use with `.png` just edit the script.
-Note that `sort -V` is used to determine image/folder ordering.
-- `out_dir` is the path where the script will place the resulting archive (called `my_archive.cbz`). It will also create a temporary directory `temp` which will be removed when finished. If it already exists the script will overwrite its content.
-
-Note that, as a side effect, filenames in `in_dir` may be changed by detox.
+- `in_dir` must be a folder containing `.jpg` files OR a folder of folders containing `.jpg` files. It won't work as expected if the folder contains a mixture of folders and images. 
+  To use with `.png` just edit the script.
+  Note that `sort -V` is used to determine image/folder ordering.
+- `out_dir` is the path where the script will place the resulting archive, called `my_archive.cbz`. If this file already exists it will ask for confirmation before deleting it. The script will also create a temporary directory `temp` which will be removed when finished. If the folder already exists the script will overwrite its content.
 
 ### Append mode
 
-You can also add images to a previously generated archive by specifying option `-m` or `--merge`. Images will be appended to the previous one. The new images are sorted with `sort -V` and the output file will be placed in the same folder as the file given in input, with name `my_archive.cbz`. If the file already exists it is replaced.
+You can also add images to a previously generated archive by specifying option `-m` or `--merge`. Images will be appended to the previous one. The new images are sorted with `sort -V` and the output file with name `my_archive.cbz` will be placed in the same folder as the file given in input. If the file already exists it is replaced.
 
 Example: `./cbzator.sh -m path_to_file.cbz new_images_dir`. 
 
 Folder `new_images_dir` must have the same format as `in_dir` described above.
-
--- cbzator.sh
+++ cbzator.sh
@@ -6,14 +6,19 @@ Usage: $(basename $0) [-h] [-m <archive_name> <input_directory>] [<input_directo
 EOM
 }
 
+# Global variables
+FILE_PATTERN='.*.[jpg|jpeg|JPG|JPEG]'
+OUT_NAME='my_archive.cbz'
+
 # Needs two arguments, the INDIR with the images to compress and OUTDIR
 createArchive () {
 	# Save input dir and output dir removing trailing /
-	local INDIR=$1
-	local OUTDIR=$2
-	local TEMPDIR=$OUTDIR/temp
+	local INDIR="$1"
+	local OUTDIR="$2"
+	local TEMPDIR="$OUTDIR/temp"
 	echo "Input dir = ${INDIR}"
 	echo "Output dir = ${OUTDIR}"
+
 	# Creates a temp/ folder inside OUTDIR if it does not exist, otherwise empties it.
 	if [ ! -d "$TEMPDIR" ]
 	then
@@ -23,12 +28,10 @@ createArchive () {
 		rm -rf $TEMPDIR/*
 		echo "Warning: ${TEMPDIR} already existed and content has been deleted."
 	fi
-	# Removes white spaces and strange characters from filenames
-	detox -r $INDIR
 
 	# Finds number of images in subfolders
-	local N_IMG=`find "${INDIR}" -maxdepth 2 -type f -regex ".*.[jpg|jpeg]" | wc -l`
-	echo "Found ${N_IMG} images."
+	local N_IMG=`find "$INDIR" -maxdepth 2 -type f -regex $FILE_PATTERN | wc -l`
+	echo "Found $N_IMG images."
 	# Finds number of digit to use to pad numbers
 	local N_DIGIT=${#N_IMG}
 
@@ -37,23 +40,26 @@ createArchive () {
 	local COUNT=1
 	# Lists all first level dirs sorted with version number sort (change sort options if something different is required)
 	# This command should return directories in the order in which they should be analyzed in order to produce the correct order of pages
-	for DIRNAME in `find "${INDIR}" -maxdepth 1 -type d | sort -V`;
-	do
-		# Iterates over filenames of images in sorted numerical order, inside DIRNAME
-		for FILENAME in `find "${DIRNAME}" -maxdepth 1 -type f -regex ".*.[jpg|jpeg]" | sort -V`;
-		do
+	# Use process substitution
+	while IFS= read -r -d '' DIRNAME; do
+		echo "Dir: $DIRNAME"
+		while IFS= read -r -d '' FILENAME; do
+			echo "File: $FILENAME"
 			# Copy and rename images inside TEMPDIR. Change 'cp' to 'mv' in order to delete original files.
 			# Also applies zero padding as necessary (e.g. 1.jpg -> 0001.jpg)
-			cp "${FILENAME}" "${TEMPDIR}/`printf \"%0${N_DIGIT}d\" ${COUNT}`.${FILENAME##*.}"
+			# Extension will be lowercase (e.g. JPG -> jpg)
+			EXT=${FILENAME##*.}
+			cp "${FILENAME}" "${TEMPDIR}/`printf \"%0${N_DIGIT}d\" ${COUNT}`.${EXT,,}"
 			# Next page will have increasing number.
 			COUNT=`expr $COUNT + 1`
-		done
-	done
+		done< <(find "$DIRNAME" -maxdepth 1 -type f -regex $FILE_PATTERN -print0 | sort -z -V)
+	done< <(find "$INDIR" -maxdepth 1 -type d -print0 | sort -z -V)
+
 	COUNT=`expr $COUNT - 1`
 	echo "Done."
-	echo "Generating cbz archive with ${COUNT} images...can take time..."
+	echo "Generating cbz archive with $COUNT images...can take time..."
 	# Produce archive with all files in dir. '-j' discard original folder structure
-	zip -j -q $OUTDIR/my_archive.cbz $TEMPDIR/*
+	zip -j -q $OUTDIR/$OUT_NAME $TEMPDIR/*
 	rm -rf $TEMPDIR
 	echo "Produced cbz file."
 	# Opens output in file manager. Uncomment if desired.
@@ -80,12 +86,10 @@ then
 	if [ ! -d "$TEMPDIR2" ]; then mkdir --parents $TEMPDIR2; else rm -rf $TEMPDIR2/*; fi
 	# Unzip all archive files into tmp1
 	unzip -j -q $2 -d $TEMPDIR1
-	# Remove spaces from filenames
-	detox -r $INDIR
 	# Move every file to add in tmp2
-	for FILENAME in `find "${INDIR}" -mindepth 1 -maxdepth 2 -type f -regex ".*.[jpg|jpeg]" | sort -V`;
-		do
-			cp "${FILENAME}" $TEMPDIR2
+	find "$INDIR" -mindepth 1 -maxdepth 2 -type f -regex $FILE_PATTERN -print0 | sort -V | \
+		while IFS= read -r -d '' FILENAME; do
+			cp "$FILENAME" $TEMPDIR2
 		done
 	createArchive "$TEMPDIR" "$OUTDIR"
 	# Delete tmp folder
@@ -93,11 +97,29 @@ then
 	exit 0
 elif [ $# -eq 2 ]
 then
-	INDIR=`realpath -s $1`
+	INDIR=`realpath -s "$1"`
 	[ ! -d "$INDIR" ] && { echo "Error: $INDIR is not a valid input directory."; exit 1; }
-	OUTDIR=`realpath -s $2`
+	OUTDIR=`realpath -s "$2"`
 	# Creates OUTDIR if it doesn't exists
-	if [ ! -d "$OUTDIR" ]; then mkdir --parents $OUTDIR; fi
+	if [ ! -d "$OUTDIR" ]; then mkdir --parents $OUTDIR;
+	else
+		# Ask for confirmation if the output file already exists
+		if [ -f "$OUTDIR/$OUT_NAME" ]
+		then
+			echo "Warning: output file '$OUTDIR/$OUT_NAME' is already present and will be deleted."
+			read -p "Do you want this to happen (y/n)? Press 'n' to abort. " -n 1 -r
+			if [[ $REPLY =~ ^[Yy]$ ]]
+			then
+			    rm -f "$OUTDIR/$OUT_NAME"
+			    echo "Deleted."
+			else
+				echo
+				echo "Aborted."
+				exit 0
+			fi
+		fi
+	fi
+
 	createArchive "$INDIR" "$OUTDIR"
 	exit 0
 else
